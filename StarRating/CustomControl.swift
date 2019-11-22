001cbf4d443ce76c0ec542ba04f3b79c044a5c9f
Craig Swanson <cswanson02@me.com>
2019-11-21 21:56:38 -0600

    UI enhancements with the star color and view background color

--- StarRating/CustomControl.swift
+++ StarRating/CustomControl.swift
@@ -17,7 +17,7 @@ class CustomControl: UIControl {
     private let componentDimension: CGFloat = 40.0
     private let componentCount: Int = 5
     private let componentActiveColor: UIColor = .black
-    private let componentInactiveColor: UIColor = .gray
+    private let componentInactiveColor: UIColor = .lightGray
     
     required init?(coder aCoder: NSCoder) {
         super.init(coder: aCoder)
